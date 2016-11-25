3a694f07fc43b896e02ee95f8e55e5ba96f80e25
Sindre Sorhus <sindresorhus@gmail.com>
2016-11-25 13:07:18 +0700

    Simplify the codebase
    Thanks to @albertstill ✨
    
    Fixes #3
    Fixes #1

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
 'use strict';
-module.exports = iterable => new Promise(resolve => {
-	const input = Array.from(iterable);
-
-	if (input.length === 0) {
-		resolve([]);
-		return;
-	}
-
-	const ret = [];
-	let i = 0;
-	let doneCount = 0;
-
-	const run = (el, i) => {
-		Promise.resolve(el)
-			.then(
-				value => {
-					ret[i] = {value};
-				},
-				reason => {
-					ret[i] = {reason};
-				}
-			).then(() => {
-				if (++doneCount === input.length) {
-					resolve(ret);
-				}
-			});
-	};
-
-	for (const el of input) {
-		run(el, i++);
-	}
-});
+module.exports = iterable => Promise.all(iterable.map(el =>
+	Promise.resolve(el).then(
+		value => ({value}),
+		reason => ({reason})
+	)
+));
