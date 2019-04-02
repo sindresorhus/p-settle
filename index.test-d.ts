71d89b55502ff39b8db3ddbfaffe2fc9a1fd138e
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-03 00:12:30 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,5 +1,6 @@
-import {expectType} from 'tsd-check';
-import pSettle, {PromiseResult} from '.';
+import {expectType} from 'tsd';
+import pSettle = require('.');
+import {PromiseResult} from '.';
 
 expectType<Promise<PromiseResult<number>[]>>(pSettle([1, Promise.resolve(2)]));
 expectType<Promise<PromiseResult<number>[]>>(
