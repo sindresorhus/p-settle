aa1a9803ac5c3822ed9057816762b177925b18ea
Valentin Hervieu <ValentinH@users.noreply.github.com>
2020-05-21 07:36:59 +0200

    Improve TypeScript types with support for arrays containing elements of multiple types (#13)

--- index.d.ts
+++ index.d.ts
@@ -17,6 +17,9 @@ declare namespace pSettle {
 	type PromiseResult<ValueType> = pReflect.PromiseResult<ValueType>;
 	type PromiseFulfilledResult<ValueType> = pReflect.PromiseFulfilledResult<ValueType>;
 	type PromiseRejectedResult = pReflect.PromiseRejectedResult;
+
+	// TODO: Use the native version when TS supports it (should be in v4).
+	type Awaited<T> = T extends undefined ? T : T extends PromiseLike<infer U> ? U : T;
 }
 
 /**
@@ -52,9 +55,9 @@ import pSettle = require('p-settle');
 })();
 ```
 */
-declare function pSettle<ValueType>(
-	array: ReadonlyArray<ValueType | PromiseLike<ValueType> | ((...args: any[]) => PromiseLike<ValueType>)>,
+declare function pSettle<ValueType extends readonly any[]>(
+	array: ValueType,
 	options?: pSettle.Options
-): Promise<Array<pSettle.PromiseResult<ValueType>>>;
+): Promise<{-readonly [P in keyof ValueType]: pSettle.PromiseResult<pSettle.Awaited<ValueType[P]>>}>;
 
 export = pSettle;
--- index.test-d.ts
+++ index.test-d.ts
@@ -6,3 +6,6 @@ expectType<Promise<Array<PromiseResult<number>>>>(pSettle([1, Promise.resolve(2)
 expectType<Promise<Array<PromiseResult<number>>>>(
 	pSettle([1, Promise.resolve(2)], {concurrency: 1})
 );
+
+expectType<Promise<[PromiseResult<number>, PromiseResult<string>]>>(pSettle([1, '2']));
+expectType<Promise<[PromiseResult<string>, PromiseResult<number>]>>(pSettle([Promise.resolve('1'), Promise.resolve(2)]));
