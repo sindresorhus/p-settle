63b72211d1e02e764f22e40ad83334fc2a78bf0f
Hiroyuki Anai <pirosikick@gmail.com>
2020-05-28 17:29:32 +0900

    Fix a type of the return value of pSettle

--- index.test-d.ts
+++ index.test-d.ts
@@ -6,6 +6,7 @@ expectType<Promise<Array<PromiseResult<number>>>>(pSettle([1, Promise.resolve(2)
 expectType<Promise<Array<PromiseResult<number>>>>(
 	pSettle([1, Promise.resolve(2)], {concurrency: 1})
 );
+expectType<Promise<Array<PromiseResult<number>>>>(pSettle([async () => 2]));
 
 expectType<Promise<[PromiseResult<number>, PromiseResult<string>]>>(pSettle([1, '2']));
 expectType<Promise<[PromiseResult<string>, PromiseResult<number>]>>(pSettle([Promise.resolve('1'), Promise.resolve(2)]));
