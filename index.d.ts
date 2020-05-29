d01d77e845e59771f92b8e7a5ceeadb57a01dec4
Hiroyuki ANAI <pirosikick@gmail.com>
2020-05-29 17:03:38 +0900

    Fix TypeScript type return value when the first argument is an array of functions (#14)

--- index.d.ts
+++ index.d.ts
@@ -17,6 +17,7 @@ declare namespace pSettle {
 	type PromiseResult<ValueType> = pReflect.PromiseResult<ValueType>;
 	type PromiseFulfilledResult<ValueType> = pReflect.PromiseFulfilledResult<ValueType>;
 	type PromiseRejectedResult = pReflect.PromiseRejectedResult;
+	type ReturnValue<T> = T extends (...args: any) => any ? ReturnType<T> : T;
 
 	// TODO: Use the native version when TS supports it (should be in v4).
 	type Awaited<T> = T extends undefined ? T : T extends PromiseLike<infer U> ? U : T;
@@ -58,6 +59,6 @@ import pSettle = require('p-settle');
 declare function pSettle<ValueType extends readonly any[]>(
 	array: ValueType,
 	options?: pSettle.Options
-): Promise<{-readonly [P in keyof ValueType]: pSettle.PromiseResult<pSettle.Awaited<ValueType[P]>>}>;
+): Promise<{-readonly [P in keyof ValueType]: pSettle.PromiseResult<pSettle.Awaited<pSettle.ReturnValue<ValueType[P]>>>}>;
 
 export = pSettle;
